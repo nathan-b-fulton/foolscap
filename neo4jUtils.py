27fd783d12f611f5c52eb1e22e7a865222b90f37
Nathan Fulton <nathan.bartholomew.fulton@gmail.com>
2024-09-15 18:37:36 -0500

    More progress on bulk rels

--- neo4jUtils.py
+++ neo4jUtils.py
@@ -45,7 +45,34 @@ def createNodeSet(node_label:str)->Result:
     return res
 
 
-def creatRelSet(node_label:str, relation: str)->Result:
+def creatRelSet(source_label:str, relation: str)->Result:
     """ """
+    graph = openGraph()
+    label_dict = label_dicts[source_label]
+    with open(label_dict['file'],"r",encoding='utf-8') as f:
+        item_list = load(f)
+    for i in item_list:
+        source = i[source_label]
+        targets = i[relation]
+        rel_def = label_dict[relation]
+        props = rel_def['props']
+        target_label = rel_def['target']
+        with graph.session(database="neo4j") as session:
+            res:Result = session.run("""
+                            WITH $targets AS targets
+                            UNWIND targets AS target
+                            MATCH (s:{} {{ {}:$source }})
+                            MATCH (t:{} {{ {}:target }})
+                            CREATE (t)-[r:{}]->(s)
+                            SET r = $props
+                            """.format(source_label, source_label, 
+                                       target_label, target_label, 
+                                       relation), 
+                            source=source, 
+                            targets=targets, 
+                            props=props
+                                )
+    graph.close()
+    return res
 
 createNodeSet('ref')
